c570b6833b35c4feb5a57db521c437b2f1292652
Tejas Jhamnani <tejasj2909@gmail.com>
2020-10-22 12:13:30 +0530

    My Portfolio Website

--- src/server.js
+++ src/server.js
@@ -3,15 +3,15 @@ const path = require("path");
 const hbs = require("hbs");
 var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport({
-  service:"gmail",
-  auth:{
-    user:"tajhamnani_b18@ce.vjti.ac.in",
-    pass:"JAIGURUDEV"
-  }
-})
+  service: "gmail",
+  auth: {
+    user: "tajhamnani_b18@ce.vjti.ac.in",
+    pass: "JAIGURUDEV",
+  },
+});
 const port = process.env.PORT || 3000;
 app = express();
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 const staticPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
 const partialPath = path.join(__dirname, "../templates/partials");
@@ -26,46 +26,46 @@ app.get("/about", (req, res) => {
   res.status(200).render("about", { title: "About Page" });
 });
 app.get("/projects", (req, res) => {
-  res.status(200).render("projects", { title: "Projects"});
-})
+  res.status(200).render("projects", { title: "Projects" });
+});
 app.get("/skills", (req, res) => {
-  res.status(200).render("skills", { title:"Skills"})
-})
+  res.status(200).render("skills", { title: "Skills" });
+});
 app.get("/contact", (req, res) => {
   res.status(200).render("contact", { title: "Contact Page" });
 });
-app.post("/contact",(req,res)=>{
+app.post("/contact", (req, res) => {
   var mailOptions1 = {
-    from:"tajhamnani_b18@ce.vjti.ac.in",
-    to:`${req.body.email}`,
-    subject:"Thanks for Submitting Your Form",
-    text : `Hi ${req.body.username}  your response has been recieved .Thanks for filling the form 🙂`
-  }
+    from: "tajhamnani_b18@ce.vjti.ac.in",
+    to: `${req.body.email}`,
+    subject: "Thanks for Submitting Your Form",
+    text: `Hi ${req.body.username}  your response has been recieved .Thanks for filling the form 🙂`,
+  };
   var mailOptions2 = {
-    from:"tajhamnani_b18@ce.vjti.ac.in",
-    to:`tajhamnani_b18@ce.vjti.ac.in`,
-    subject:"SomeOne submitted Your Form",
-    text : `${req.body.username} filled your website form .Email is ${req.body.email} and number is ${req.body.phone} and message is ${req.body.msg}`
-  }
-  transporter.sendMail(mailOptions1,function(err,info){
-    if(err){
+    from: "tajhamnani_b18@ce.vjti.ac.in",
+    to: `tajhamnani_b18@ce.vjti.ac.in`,
+    subject: "SomeOne submitted Your Form",
+    text: `${req.body.username} filled your website form .Email is ${req.body.email} and number is ${req.body.phone} and message is ${req.body.msg}`,
+  };
+  transporter.sendMail(mailOptions1, function (err, info) {
+    if (err) {
       console.log(err);
+    } else {
+      console.log("Email Sent " + info.response);
     }
-    else{
-      console.log("Email Sent "+info.response );
-    }
-  })
-  transporter.sendMail(mailOptions2,function(err,info){
-    if(err){
+  });
+  transporter.sendMail(mailOptions2, function (err, info) {
+    if (err) {
       console.log(err);
+    } else {
+      console.log("Email Sent " + info.response);
     }
-    else{
-      console.log("Email Sent "+info.response );
-    }
-  })
-console.log(req.body);
-res.status(200).render("submit", { title: "Thank You" ,name:req.body.username});
-})
+  });
+  console.log(req.body);
+  res
+    .status(200)
+    .render("submit", { title: "Thank You", name: req.body.username });
+});
 app.get("*", (req, res) => {
   res.status(404).render("404", { title: "Page Not Found" });
 });
